b370df01011169d16e405ecb66d19b7640f83cf5
Kauã Hilgenberg <kauahilgenbergg@gmail.com>
2025-12-15 14:43:50 -0300

    first commit

---
+++ backend/incorporate/src/main/java/com/patrimonio/incorporate/IncorporateApplication.java
@@ -0,0 +1,13 @@
+package com.patrimonio.incorporate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncorporateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncorporateApplication.class, args);
+	}
+
+}
---
+++ backend/incorporate/src/test/java/com/patrimonio/incorporate/IncorporateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patrimonio.incorporate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncorporateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
